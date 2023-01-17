b59603ead11e83c17edd548536d40233a8655159
felipe <fmagven93@gmail.com>
2023-01-17 10:19:56 -0300

    ola spring

---
+++ src/main/java/com/olaspring/olaspring/OlaspringApplication.java
@@ -0,0 +1,13 @@
+package com.olaspring.olaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlaspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OlaspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/olaspring/olaspring/OlaspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olaspring.olaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlaspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
